d30029b4ee4e339b609e57221f6daef191b6fb3e
Felipe A de Souza <felipe.az.souza08@gmail.com>
2021-07-01 18:52:57 -0300

    nova página de gráfico

---
+++ lib/Telas/HomeScreens/grafic.dart
@@ -0,0 +1,4 @@
+import 'package:flutter/material.dart';
+import 'package:fl_chart/fl_chart.dart';
+import 'package:font_awesome_flutter/font_awesome_flutter.dart';
+import 'package:cloud_firestore/cloud_firestore.dart';
