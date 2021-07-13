604466aba2430a9c911953feccf199d1c0bb0d5b
Felipe A de Souza <felipe.az.souza08@gmail.com>
2021-07-13 20:46:46 -0300

    teste

--- lib/Telas/HomeScreens/stats.dart
+++ lib/Telas/HomeScreens/stats.dart
@@ -1,5 +1,4 @@
 import 'package:firebase_auth/firebase_auth.dart';
-import 'package:firebase_core/firebase_core.dart';
 import 'package:flutter/material.dart';
 import 'package:syncfusion_flutter_charts/charts.dart';
 // ignore: unused_import
@@ -17,72 +16,87 @@ class StatsPage extends StatefulWidget {
 class _StatsPageState extends State<StatsPage> {
   FirebaseFirestore db = FirebaseFirestore.instance;
   final user = FirebaseAuth.instance.currentUser!;
-  late List<GDPData> _chartData = [];
+  List<GDPData> _chartData = [];
   late TooltipBehavior _tooltipBehavior;
   late int contMedit;
   late int contCromo;
   late int contMusic;
-  late int i;
-  late List<String> emotea = []; 
+  late List<String> emotea = [];
   late String emoteBase;
 
   @override
-  void initState(){
-     readFirebase();
-    _tooltipBehavior = TooltipBehavior(enable: true);  
+  void initState() {
+    readFirebase();
+    _tooltipBehavior = TooltipBehavior(enable: true);
     super.initState();
   }
 
   @override
-  Widget build(BuildContext context) {
-    return SafeArea(
-        child: Scaffold(
-      backgroundColor: Colors.black87,
-      body: SfCircularChart(
-        title: ChartTitle(
-          text: 'Estatísticas Gerais \n Conforme uso por emoção sentida',
-          textStyle: TextStyle(color: Colors.white),
-        ),
-        legend: Legend(
-            isVisible: true,
-            overflowMode: LegendItemOverflowMode.wrap,
+  Widget build(BuildContext context) => SafeArea(
+          child: Scaffold(
+        backgroundColor: Colors.black87,
+        body: SfCircularChart(
+          title: ChartTitle(
+            text:
+                'Estatísticas da Meditação \n Conforme uso por emoção sentida',
             textStyle: TextStyle(color: Colors.white),
-            alignment: ChartAlignment.center,
-            itemPadding: 20),
-        tooltipBehavior: _tooltipBehavior,
-        series: <CircularSeries>[
-          DoughnutSeries<GDPData, String>(
-              dataSource: _chartData,
-              xValueMapper: (GDPData data, _) => data.emoteBase,
-              yValueMapper: (GDPData data, _) => data._contMedit,
-              pointColorMapper: (GDPData data, _) => data.colorgraf,
-              dataLabelSettings: DataLabelSettings(isVisible: true),
-              enableTooltip: true)
-        ],
-      ),
-    ));
-  }
+          ),
+          legend: Legend(
+              isVisible: true,
+              overflowMode: LegendItemOverflowMode.wrap,
+              textStyle: TextStyle(color: Colors.white),
+              alignment: ChartAlignment.center,
+              itemPadding: 20),
+          tooltipBehavior: _tooltipBehavior,
+          series: <CircularSeries>[
+            DoughnutSeries<GDPData, dynamic>(
+                dataSource: _chartData,
+                xValueMapper: (GDPData data, _) => data.emoteBase,
+                yValueMapper: (GDPData data, _) => data.contMedit,
+                pointColorMapper: (GDPData data, _) => data.colorgraf,
+                dataLabelSettings: DataLabelSettings(isVisible: true),
+                enableTooltip: true),
+          ],
+        ),
+      ));
 
-  List<GDPData> getChartData(){
-    print(emotea);
-    print('CEBOLACEBOLACEBOLACEBOLACEBOLACEBOLACEBOLACEBOLACEBOLACEBOLACEBOLA');
-    final List<GDPData> chartData = [];  
-    //for (String emoteBase in emotea){
+  List<GDPData> getChartData() {
+    print('TESTETESTETESTETESTETESTETESTETESTETESTETESTE');
+    final List<GDPData> chartData = [];
     this.emotea.forEach((emoteBase) => {
-      if (emoteBase == 'Medo') {
-        print("123"),
-        chartData.add (new GDPData(emoteBase, contMedit, Colors.blue)),
-        print(chartData),
-      } else if (emoteBase == 'Raiva') {
-        chartData.add(new GDPData(emoteBase, contMedit, Color(0xFFF06292))),       
-      } else if (emoteBase == 'Ansiedade') {
-        chartData.add(new GDPData(emoteBase, contMedit, Color(0xFF512DA8))),
-      } else if (emoteBase == 'Tristeza') {
-        chartData.add(new GDPData(emoteBase, contMedit, Color(0xffEF9A9A))),
-      } else if (emoteBase == 'Estresse') {
-        chartData.add(new GDPData(emoteBase, contMedit, Color(0xffff9900))),
-      }
-    });  
+          print(emoteBase),
+          if (emoteBase == 'Medo')
+            {
+              chartData.add(GDPData('Medo', contMedit, Colors.cyan)),
+              print(chartData),
+              print(contMedit),
+            }
+          else if (emoteBase == 'Raiva')
+            {
+              chartData
+                  .add(GDPData('Raiva', contMedit, Colors.lightGreenAccent)),
+              print(chartData),
+              print(contMedit),
+            }
+          else if (emoteBase == 'Ansiedade')
+            {
+              chartData.add(GDPData('Ansiedade', contMedit, Colors.white60)),
+              print(chartData),
+              print(contMedit),
+            }
+          else if (emoteBase == 'Triste')
+            {
+              chartData.add(GDPData('Triste', contMedit, Colors.orangeAccent)),
+              print(chartData),
+              print(contMedit),
+            }
+          else if (emoteBase == 'Estresse')
+            {
+              chartData.add(GDPData('Estresse', contMedit, Colors.deepPurple)),
+              print(chartData),
+              print(contMedit),
+            }
+        });
     return chartData;
   }
 
@@ -91,49 +105,51 @@ class _StatsPageState extends State<StatsPage> {
         .collection(user.email.toString())
         .doc('Stats')
         .get();
-    print(contMedit1.data());
     contMedit = await contMedit1.data()?['contMedit'];
+    print(contMedit);
     var contCromo1 = await FirebaseFirestore.instance
         .collection(user.email.toString())
         .doc('Stats')
         .get();
     contCromo = await contCromo1.data()?['contCromo'];
+    print(contCromo);
     var contMusic1 = await FirebaseFirestore.instance
         .collection(user.email.toString())
         .doc('Stats')
         .get();
     contMusic = await contMusic1.data()?['contMusic'];
+    print(contMusic);
     var emote = await FirebaseFirestore.instance
         .collection(user.email.toString())
         .doc('Emotion')
         .get();
     print(emote.data());
     List<String> emotea = [];
-    if ((emote.data()?['med'])==true) {
+    if ((emote.data()?['med']) == true) {
       emotea.add('Medo');
-    } if ((emote.data()?['ansi'])==true) {
+    }
+    if ((emote.data()?['ansi']) == true) {
       emotea.add('Ansiedade');
-    } if ((emote.data()?['raiva'])==true) {
+    }
+    if ((emote.data()?['raiva']) == true) {
       emotea.add('Raiva');
-    } if ((emote.data()?['stress'])==true) {
+    }
+    if ((emote.data()?['stress']) == true) {
       emotea.add('Estresse');
-    } if ((emote.data()?['triste'])==true) {
+    }
+    if ((emote.data()?['triste']) == true) {
       emotea.add('Triste');
     }
     print(emotea);
     this.emotea = emotea;
-    this._chartData = getChartData();
+    _chartData = getChartData();
+    print("testetestetestetestetestetestetestetestetesteteste, $_chartData");
   }
-  /*emote123(emote);
-    Future<void> emote123(DocumentSnapshot<Map<String, dynamic>> emote) async {
-      String _emote = emote.data()?[];
-    }*/
 }
 
-
 class GDPData {
-  String emoteBase;
-  final int _contMedit;
+  final String emoteBase;
+  final int contMedit;
   final Color colorgraf;
-  GDPData(this.emoteBase, this._contMedit, this.colorgraf);
+  GDPData(this.emoteBase, this.contMedit, this.colorgraf);
 }
